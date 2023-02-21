40b749158ad82c26387362bda9efd0b5c51aba73
Deepraj <83288891+deepraj21@users.noreply.github.com>
2023-02-21 23:06:43 +0530

    Update tetxtospeech.py

--- tetxtospeech.py
+++ tetxtospeech.py
@@ -1,27 +1,20 @@
 import tkinter as tk
 import pyttsx3
 
-
 def speak_text():
     engine = pyttsx3.init()
     engine.say(text.get())
     engine.runAndWait()
-
-
-# Create a tkinter window
+    
 window = tk.Tk()
 window.title("Text to Speech")
 window.geometry("400x200")
 
-# Create a label and an entry widget for user input
 tk.Label(window, text="Enter text:", font=("Times New Roman", 14)).pack(pady=10)
 text = tk.Entry(window, font=("Times New Roman", 14))
 text.pack(pady=10)
 
-# Create a button to trigger text-to-speech conversion
-button = tk.Button(window, text="Speak", font=(
-    "Arial", 14), command=speak_text)
+button = tk.Button(window, text="Speak", font=("Arial", 14), command=speak_text)
 button.pack(pady=10)
 
-# Run the tkinter event loop
 window.mainloop()
